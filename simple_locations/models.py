190b97082ef94c41ea6bc7dc01a5fad4e9592d3d
Sean Ting <sean.y.ting@gmail.com>
2017-03-29 12:50:29 +0900

    merge fixes

--- simple_locations/models.py
+++ simple_locations/models.py
@@ -22,7 +22,7 @@ class Point(models.Model):
     latitude = models.DecimalField(max_digits=13, decimal_places=10)
     longitude = models.DecimalField(max_digits=13, decimal_places=10)
 
-    def __unicode__(self):
+    def __str__(self):
         return _(u"%(lat)s, %(lon)s") % {'lat': self.latitude,
                                          'lon': self.longitude}
 
@@ -37,7 +37,7 @@ class AreaType(models.Model):
     name = models.CharField(max_length=100)
     slug = models.CharField(max_length=30, unique=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return _(self.name)
 
 
@@ -95,7 +95,7 @@ class Area(MPTTModel):
                     'parent': self.parent.display_name_and_type()
                 }
 
-    def __unicode__(self):
+    def __str__(self):
         ''' print Area name from its Kind and parent
 
         Example: Bamako '''
@@ -108,81 +108,3 @@ class Area(MPTTModel):
         #                                              'area': self.name,}
 
         return self.name
-
-################################
-# imported from health_facility
-
-
-class FacilityType(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.CharField(max_length=30, unique=True)
-
-    class Meta:
-        verbose_name = __("Facility Type")
-        verbose_name_plural = __("Facility Types")
-        app_label = 'simple_locations'
-
-    def __unicode__(self):
-        return self.name
-
-
-class Facility(models.Model):
-    name = models.CharField(max_length=100)
-    code = models.CharField(max_length=64, blank=True, null=False)
-    type = models.ForeignKey(FacilityType, blank=True, null=True)
-
-    catchment_areas = models.ManyToManyField(Area, related_name='catchment')
-    location = models.ForeignKey(Point, null=True, blank=True)
-    area = models.ForeignKey(Area, null=True, blank=True, related_name='facility')
-
-    parent = models.ForeignKey('self', null=True, blank=True, related_name='facility')
-
-    class Meta:
-        verbose_name = __("Facility")
-        verbose_name_plural = __("Facilities")
-        app_label = 'simple_locations'
-
-    def __unicode__(self):
-        return u"%s %s" % (self.type, self.name)
-
-    def is_root(self):
-        if self.parent is None:
-            return True
-        else:
-            return False
-
-    def get_children(self):
-        children = self._default_manager.filter(parent=self)
-        return children
-
-    def get_descendants(self):
-        descendants = children = self.get_children()
-
-        for child in children:
-            if child.has_children:
-                descendants = descendants | child.descendants
-        return descendants
-
-    @property
-    def is_child_node(self):
-        children = self._default_manager.filter(parent=self).count()
-        if children > 0:
-            return False
-        else:
-            return True
-
-    @property
-    def has_children(self):
-        children = self._default_manager.filter(parent=self).count()
-        if children > 0:
-            return True
-        else:
-            return False
-
-    @property
-    def children(self,):
-        return self.get_children()
-
-    @property
-    def descendants(self,):
-        return self.get_descendants()
