1b7f1011337fe96690c915addc160b533402c7c4
PeteCoward <peter@catalpa.io>
2017-04-03 15:29:26 +0900

    Pypi version 2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simple_locations',
-    version='2.0',
+    version='2.1',
     license="BSD",
 
     description="The common location package used for catalpa's projects.",
