d47bc28c74cb7353c9ec30582c3ab822d0f370e8
Raphael Merx <raphael.merx@gmail.com>
2017-05-08 10:32:53 +0900

    dump_locations command: fix syntax error in Python 3

--- simple_locations/management/commands/dump_locations.py
+++ simple_locations/management/commands/dump_locations.py
@@ -1,8 +1,3 @@
-import os
-import pwd
-import traceback
-from optparse import OptionParser, make_option
-
 from django.core.management.base import BaseCommand
 from simple_locations.models import Area
 
@@ -11,6 +6,6 @@ class Command(BaseCommand):
     help = """prints all districts in a form that's easy to import into excel"""
 
     def handle(self, **options):
-        print "id\tname\ttype\tparent_id\tparent_name\tparent_type"
+        print('id\tname\ttype\tparent_id\tparent_name\tparent_type')
         for a in Area.tree.all():
-            print "%d\t%s\t%s\t%d\t%s\t%s" % (a.pk, a.name, a.kind.name, a.parent.pk if a.parent else -1, a.parent.name if a.parent else '', a.parent.kind.name if a.parent else '')
+            print("%d\t%s\t%s\t%d\t%s\t%s" % (a.pk, a.name, a.kind.name, a.parent.pk if a.parent else -1, a.parent.name if a.parent else '', a.parent.kind.name if a.parent else ''))
