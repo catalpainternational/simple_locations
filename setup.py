dab698dcfa29470b2cd2aff991372f7756bf0c77
PeteCoward <peter@catalpa.io>
2018-02-02 16:01:10 +0900

    bump pypi version number to 2.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simple_locations',
-    version='2.3',
+    version='2.5',
     license="BSD",
 
     description="The common location package used for catalpa's projects.",
