02d064206a4f411e1c062a2eed5a3fa1b60c90dc
Roberto Maurizzi <roberto@catalpa.io>
2019-03-05 17:46:06 +0900

    Inline with children, improve map widget

--- simple_locations/admin.py
+++ simple_locations/admin.py
@@ -2,7 +2,8 @@
 # encoding=utf-8
 # maintainer rgaudin
 
-from django.contrib import admin
+from django.conf import settings
+from django.contrib.gis import admin
 from django import forms
 
 from mptt.admin import MPTTModelAdmin
@@ -27,11 +28,26 @@ class AreaTypeAdmin(admin.ModelAdmin):
     list_display = ('slug', 'name')
 
 
-class AreaAdmin(MPTTModelAutocompleteAdmin):
-    list_display = ( 'name', 'kind', 'location', 'code')
+class AreadChildrenInline(admin.TabularInline):
+    model = Area
+    fields = ['name', 'code', 'kind']
+    show_change_link = True
+    extra = 0
+
+
+class AreaAdmin(MPTTModelAutocompleteAdmin, admin.OSMGeoAdmin):
+    # default_lon = getattr(settings, 'GIS_DEFAULT_LAT', -8.8742)
+    # default_lat = getattr(settings, 'GIS_DEFAULT_LON', 125.7275)
+    default_lon = -8.8742
+    default_lat = 125.7275
+    default_zoom = 16
+    # debug = True  - enable if copy/pasting WKTs is useful
+    units = 'km'
+    list_display = ('name', 'kind', 'location', 'code')
     search_fields = ['code', 'name']
     list_filter = ('kind',)
     related_search_fields = {'parent': ('^name',)}
+    inlines = [AreadChildrenInline]
 
 
 admin.site.register(Point, PointAdmin)
