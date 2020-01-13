52301ea41f113128a38b105c89b5d286ae28fb52
Joshua Brooks <josh.vdbroek@gmail.com>
2020-01-13 12:26:57 +0900

    v 2.75.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simple_locations',
-    version='2.75',
+    version='2.75.1',
     license="BSD",
 
     description="The common location package used for catalpa's projects.",
