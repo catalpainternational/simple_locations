66ddf4d5884d70d1fe547e98d6819b88cad64975
Josh Brooks <josh@catalpa.io>
2022-02-25 10:40:32 +0900

    correct settings for black

--- simple_locations/wsgi.py
+++ simple_locations/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_locations.tests.test_settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simple_locations.tests.test_settings")
 
 application = get_wsgi_application()
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-
 ]
 
 ROOT_URLCONF = "simple_locations.urls"
