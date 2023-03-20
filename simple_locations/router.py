2cf1b114e5cbc9909fef674e1af62d0a7f8aed11
Josh Brooks <josh@catalpa.io>
2023-03-20 13:58:30 +0900

    added pydantic endpoints

---
+++ simple_locations/router.py
@@ -0,0 +1,46 @@
+from typing import List
+from ninja import Router
+from simple_locations import schemas, model_schemas, models
+
+router = Router(tags=["SimpleLocations"])
+
+
+@router.get("/area/list.json", response=List[model_schemas.AreaModelSchema])
+def area_list(request):
+    """
+    Returns a list of area information: name, id, parent id
+    """
+    return models.Area.objects.all()
+
+
+@router.get("/areatype/list.json", response=List[model_schemas.AreaTypeModelSchema])
+def area_type_list(request):
+    """
+    List the different area types
+    """
+    return models.AreaType.objects.all()
+
+
+@router.get("/area/by-id/{area_id}.geojson", response=schemas.Feature)
+def area_id(request, area_id: int):
+    """
+    Returns the geometry of a single Area
+    as a single GeoJSON Feature
+    """
+    return schemas.Feature.parse_obj(models.Area.geofunctions.filter(pk=area_id).to_features().first().feature)
+
+
+@router.get("/area/by-parent/{area_id}.geojson", response=schemas.FeatureCollection)
+def area_children(request, area_id: int):
+    """
+    Returns the direct descendants of a given Area as a FeatureCollection
+    """
+    return models.Area.geofunctions.filter(parent=area_id).to_featurecollection()
+
+
+@router.get("/area/by-type/{area_type}.geojson", response=schemas.FeatureCollection)
+def area_type(request, area_type: str):
+    """
+    Returns all areas of a given type
+    """
+    return models.Area.geofunctions.filter(kind__slug=area_type).to_featurecollection()
