3330509dab497a48a0ddb81d06fc61509fdec666
RobertoMaurizzi <roberto@catalpa.io>
2025-06-27 15:40:54 +0800

    import JSONObject from 'proper' module

--- simple_locations/gis_functions.py
+++ simple_locations/gis_functions.py
@@ -5,7 +5,7 @@ from django.contrib.gis.db.models.functions import AsGeoJSON as AsGeoJson_
 from django.db import models
 from django.db.models import F, Func, Value
 from django.db.models.fields.json import JSONField
-from django.db.models.functions.comparison import JSONObject
+from django.db.models.functions import JSONObject
 
 
 class Quantize(models.Func):
